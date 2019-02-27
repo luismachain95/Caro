54ccf650fce3a895ed5418b0803f5ba0bcf79e4c
Luis <luismachain95@hotmail.com>
2019-02-27 01:32:45 -0300

    Updates

---
+++ precache-manifest.245e18cf40faa39fd961690544fbc10e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d782084d584547e388f6",
+    "url": "/caro/static/js/runtime~main.d782084d.js"
+  },
+  {
+    "revision": "6597d0b590922e7fc867",
+    "url": "/caro/static/js/main.6597d0b5.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/caro/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6597d0b590922e7fc867",
+    "url": "/caro/static/css/main.c661b515.chunk.css"
+  },
+  {
+    "revision": "2077b910444a931f186c3553a0b7481f",
+    "url": "/caro/index.html"
+  }
+];
